675c488f912ade12d2929cd8f982f21c54506165
Amir <amir@example.com>
2022-07-03 13:43:03 +0430

    Add maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
